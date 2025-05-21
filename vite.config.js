7d803db24642498e339b2d0cd15979e9d5eec237
hsuyunyun <5b1g0011A@stust.edu.tw>
2025-05-21 12:44:15 +0800

    123

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/521-react-app/' // ← 必須正確設定，GitHub Pages 才找得到資源
+  base: '/521-react-app/'  // <- 根據 GitHub Repo 名稱設定
 })
