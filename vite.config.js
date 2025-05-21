aced6774be9126b0c47fa8b2c9091e63660a6322
hsuyunyun <5b1g0011A@stust.edu.tw>
2025-05-21 11:52:35 +0800

    package.json有問題

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/521-react-app/' // ← 必須正確設定，GitHub Pages 才找得到資源
 })
