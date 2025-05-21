a8c2e600543b1b690729088b43c80a368c9d9389
hsuyunyun <5b1g0011A@stust.edu.tw>
2025-05-21 13:00:47 +0800

    svg改路徑

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import viteLogo from "/521-react-app/vite.svg";
 import "./App.css";
 import MySlider from "./component/MySlider";
 
