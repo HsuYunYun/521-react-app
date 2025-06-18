a8420e9c77e3fab99f67d956f82909a8f496f6ea
hsuyunuyn <5b1g0011@stust.edu.tw>
2025-06-18 12:58:22 +0800

    移除計算機與井字遊戲元件，清理不必要的代碼

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,6 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 import MySlider from "./component/rgb-panel/MySlider";
 import MyPanel from "./component/rgb-panel/MyPanel";
-import MyCalculator from "./component/calculator/MyCalculator";
-import TicTacToe from "./component/tic-tac-toe/TicTacToe";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -39,18 +37,6 @@ function App() {
       <br />
       <h2>第二個元件：RGB色彩面板</h2>
       <MyPanel />
-      <h2>第三個元件:計算機</h2>
-      <MyCalculator />
-      <h2>第四個元件:井字遊戲</h2>
-      <TicTacToe />
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
